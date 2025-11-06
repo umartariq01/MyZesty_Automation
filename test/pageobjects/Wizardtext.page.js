1154473e0f70d3a388744f4b1c1054f306a14173
umer.ejaz <umer.ejaz@msd-my.com>
2025-11-06 12:20:45 +0500

    Wizard text Code

---
+++ test/pageobjects/Wizardtext.page.js
@@ -0,0 +1,171 @@
+import { $, browser } from "@wdio/globals";
+
+class WizardText {
+  get wizardEditor() {
+    return $(
+      `//android.widget.Button[@content-desc="wizardeditor"]/android.view.ViewGroup`
+    );
+  }
+  get images_tab() {
+    return $(
+      '//android.widget.TextView[@resource-id="com.myzesty:id/title" and @text="Images"]'
+    );
+  }
+  get video_tab() {
+    return $(
+      '//android.widget.TextView[@resource-id="com.myzesty:id/title" and @text="Videos"]'
+    );
+  }
+  get done_button() {
+    return $(
+      '//android.widget.FrameLayout[@resource-id="com.myzesty:id/done"]'
+    );
+  }
+  get apply_changes() {
+    return $('//android.widget.ImageView[@resource-id="com.myzesty:id/done"]');
+  }
+  get advance_add() {
+    return $(
+      '//android.widget.ImageView[@resource-id="com.myzesty:id/addVideo"]'
+    );
+  }
+  get close_project() {
+    return $(
+      '//android.widget.ImageView[@resource-id="com.myzesty:id/cancel"]'
+    );
+  }
+  get open_draft() {
+    return $(
+      '(//android.widget.ImageView[@resource-id="com.myzesty:id/img"])[1]'
+    );
+  }
+  get export() {
+    return $(
+      '//android.widget.FrameLayout[@resource-id="com.myzesty:id/export"]'
+    );
+  }
+  get export_done() {
+    return $('//android.view.ViewGroup[@content-desc="Done"]');
+  }
+  get album() {
+    return $(
+      '//android.widget.TextView[@resource-id="com.myzesty:id/album_name"]'
+    );
+  }
+  get automation_album() {
+    return $(
+      '//android.widget.TextView[@resource-id="com.myzesty:id/tv_folder_name" and @text="Automation"]'
+    );
+  }
+  get go_back() {
+    return $(
+      '//android.widget.FrameLayout[@resource-id="com.myzesty:id/back"]/android.widget.ImageView'
+    );
+  }
+  get wizard_popup() {
+    return $('//android.widget.TextView[@text="Start Creating"]');
+  }
+  get text_tab() {
+    return $(
+      '//android.widget.LinearLayout[@resource-id="com.myzesty:id/text"]'
+    );
+  }
+  get color_tab() {
+    return $('//android.widget.TextView[@text="Color"]');
+  }
+  get style_tab() {
+    return $('//android.widget.TextView[@text="Style"]');
+  }
+  get stroke_tab() {
+    return $('//android.widget.TextView[@text="Stroke"]');
+  }
+  get label_tab() {
+    return $('//android.widget.TextView[@text="Label"]');
+  }
+  get text_duration() {
+    return $(
+      '//android.widget.ImageView[@resource-id="com.myzesty:id/full_length_icon"]'
+    );
+  }
+  get duration_toast()
+  {
+    return $('//android.widget.TextView[@resource-id="com.myzesty:id/wizardToolTip"]');
+  }
+  async Open_WizardEditor() {
+    await this.wizardEditor.click();
+  }
+  async Click_Img_Tab() {
+    await this.images_tab.click();
+  }
+  async Click_Video_Tab() {
+    await this.video_tab.click();
+  }
+  async Click_Advance_Add() {
+    await this.advance_add.click();
+    await browser.pause(500);
+  }
+  async Click_Album() {
+    await this.album.click();
+  }
+  async Click_Automation_Album() {
+    await this.automation_album.click();
+  }
+  async Click_Close_Project() {
+    await (await this.close_project).click();
+  }
+  async Open_Draft_Proj() {
+    await (await this.open_draft).click();
+  }
+  async Click_Done_Btn() {
+    await this.done_button.click();
+    await browser.pause(2000);
+  }
+  async Apply_Changes() {
+    await this.apply_changes.click();
+  }
+  async Toolbar_Back() {
+    await this.go_back.click();
+  }
+  async Export_Media() {
+    await this.export.click();
+  }
+  async Export_Done_Btn() {
+    const isVisible = await this.export_done.waitForDisplayed();
+    if (isVisible) {
+      await this.export_done.click();
+    } else {
+      console.log("Done Button not visible.");
+    }
+  }
+  async Select_Media_1(index = 1) {
+    const selector = `(//android.view.View[@resource-id="com.myzesty:id/hover"])[${index}]`;
+    await $(selector).click();
+  }
+  async Verify_Wizard_PopUp() {
+    const isVisible = await this.wizard_popup.isExisting();
+    if (isVisible) {
+      await this.wizard_popup.click();
+    } else {
+      console.log("Wizard Popup is not displayed.");
+    }
+  }
+  async Click_Text_Tab() {
+    await this.text_tab.click();
+  }
+  async Click_Color_Tab() {
+    await this.color_tab.click();
+  }
+  async Click_Style_Tab() {
+    await this.style_tab.click();
+  }
+  async Click_Stroke_Tab() {
+    await this.stroke_tab.click();
+  }
+  async Click_Label_Tab() {
+    await this.label_tab.click();
+  }
+  async Apply_Text_Duration() {
+    await this.text_duration.click();
+  }
+}
+export default new WizardText();
---
+++ test/specs/test.wizardtext.js
@@ -0,0 +1,226 @@
+import WizardText from "../pageobjects/Wizardtext.page.js";
+import Slider from "../pageobjects/Slider.page.js";
+import Subscription from "../pageobjects/BuyPremium.page.js";
+import Common_function from "../pageobjects/commonfun.page.js";
+import { browser, expect } from "@wdio/globals";
+import assert from "assert";
+import commonfunPage from "../pageobjects/commonfun.page.js";
+
+describe("Wizard Text Feature Test Suite", () => {
+  before(async () => {
+    console.log(
+      "===== Running pre-checks before Wizard Text Feature Tests ====="
+    );
+
+    await Subscription.Check_Subscription("Processing");
+  });
+
+  after(async () => {
+    console.log("===== Wizard Text Feature Test Suite finished =====");
+  });
+
+  it("Verify that user can access the Text editor from Wizard Editor and the keyboard appears for typing.", async () => {
+    await WizardText.Open_WizardEditor();
+    await WizardText.Click_Album();
+    await WizardText.Click_Automation_Album();
+    await WizardText.Click_Img_Tab();
+    await Common_function.selectImages(3);
+    // await WizardText.Select_Media_1(3);
+    await WizardText.Click_Video_Tab();
+    await WizardText.Select_Media_1();
+    await WizardText.Click_Done_Btn();
+    await Common_function.waitForElementToBeVisible(
+      '//android.widget.ImageView[@resource-id="com.myzesty:id/play"]'
+    );
+    await WizardText.Click_Text_Tab();
+    await Common_function.waitForElementToBeVisible(
+      '//android.widget.EditText[@resource-id="com.myzesty:id/text_area"]'
+    );
+    await Common_function.clickElementByXPath(
+      // Click on text area to bring up keyboard
+      '//android.widget.EditText[@resource-id="com.myzesty:id/text_area"]'
+    );
+    await browser.keys("Script Running");
+    await WizardText.Apply_Changes();
+  });
+
+  it("Verify that the help/instruction icon displays duration guidance popup.", async () => {
+    await Common_function.clickElementByXPath(
+      // Open Guide icon
+      '//android.widget.ImageView[@resource-id="com.myzesty:id/showGuide"]'
+    );
+    await browser.pause(1000);
+    await Slider.tapScreen(527, 512);
+  });
+
+  it("Verify that user can change the font and new fonts download when selected.", async () => {
+    await Common_function.waitForElementToBeVisible(
+      // Wait for Font Tab to load
+      '//android.widget.TextView[@text="Font"]'
+    );
+    await Common_function.clickElementByXPath(
+      // Select Font style
+      '(//android.widget.ImageView[@resource-id="com.myzesty:id/image"])[2]'
+    );
+    await browser.pause(1000);
+  });
+
+  it("Verify that user can change the text fill color using the color slider.", async () => {
+    await WizardText.Click_Color_Tab();
+    await Slider.Slider(36, 989, 1823, 1916, 0.4);
+    await browser.pause(1000);
+    await Slider.Slider(196, 955, 1916, 2026, 0.8);
+    await browser.pause(1000);
+  });
+
+  it("Verify that user can apply bold and italic text styles.", async () => {
+    await WizardText.Click_Style_Tab();
+    await Common_function.clickElementByXPath(
+      // Bold text
+      '//android.widget.ImageView[@resource-id="com.myzesty:id/bold"]'
+    );
+    await Common_function.clickElementByXPath(
+      // Italic text
+      '//android.widget.ImageView[@resource-id="com.myzesty:id/italic"]'
+    );
+  });
+
+  it("Verify that shadow intensity slider adds and adjusts text shadow.", async () => {
+    await Slider.Slider(199, 955, 1988, 2098, 0.6);
+    await browser.pause(1000);
+  });
+
+  it("Verify that user can adjust text stroke thickness and color.", async () => {
+    await WizardText.Click_Stroke_Tab();
+    await Slider.Slider(133, 955, 1823, 1933, 0.5);
+    await browser.pause(700);
+    await Slider.Slider(38, 989, 2019, 2112, 0.3);
+    await browser.pause(700);
+  });
+
+  it("Verify that label backgrounds can be applied behind text using Label A and Label B options.", async () => {
+    await WizardText.Click_Label_Tab();
+    await Common_function.clickElementByXPath(
+      // Select Label
+      '//android.widget.ImageView[@resource-id="com.myzesty:id/background"]'
+    );
+  });
+
+  it("Verify that user can adjust label color and background radius.", async () => {
+    await Slider.Slider(36, 989, 1988, 2018, 0.4);
+    await browser.pause(700);
+    await Slider.Slider(175, 955, 2081, 2191, 0.5);
+    await browser.pause(400);
+    await WizardText.Apply_Changes();
+    await browser.pause(1000);
+  });
+
+  it("Verify that tapping the close (X) icon discards entered text.", async () => {
+    await WizardText.Click_Text_Tab();
+    await Common_function.waitForElementToBeVisible(
+      '//android.widget.EditText[@resource-id="com.myzesty:id/text_area"]'
+    );
+    await browser.keys("Test Script");
+    // Discard the entered text
+    await WizardText.Click_Close_Project();
+    await browser.pause(1000);
+  });
+
+  it("Verify that tapping the cross icon on text removes it from media.", async () => {
+    await WizardText.Click_Text_Tab();
+    await Common_function.waitForElementToBeVisible(
+      '//android.widget.EditText[@resource-id="com.myzesty:id/text_area"]'
+    );
+    await browser.keys("Check text removal");
+    await WizardText.Apply_Changes();
+    await browser.pause(1000);
+    await WizardText.Apply_Changes();
+    // Select the Text
+    await Common_function.clickElementByXPath(
+      '(//android.widget.FrameLayout[@resource-id="com.myzesty:id/container"])[2]'
+    );
+    // Remove text (Cross button on text)
+    await Common_function.clickElementByXPath(
+      '//android.widget.ImageView[@resource-id="com.myzesty:id/delete_icon"]'
+    );
+    await browser.pause(1000);
+  });
+
+  it("Verify that text can be applied to entire duration or to current media only using the circle icon.", async () => {
+    // Select the Text
+    await Common_function.clickElementByXPath(
+      '//android.widget.FrameLayout[@resource-id="com.myzesty:id/container"]'
+    );
+    await WizardText.Apply_Text_Duration();
+    const toastMessage = await WizardText.duration_toast.getText();
+    assert.strictEqual(
+      toastMessage,
+      "Text applied to the entire duration.",
+      "Duration toast message did not match"
+    );
+    await browser.pause(1000);
+    await WizardText.Apply_Text_Duration();
+    const toastMessage2 = await WizardText.duration_toast.getText();
+    assert.strictEqual(
+      toastMessage2,
+      "Text applied to the current media.",
+      "Duration toast message did not match"
+    );
+    await browser.pause(1000);
+    await WizardText.Apply_Changes();
+  });
+
+  it("Verify that text can be applied to multiple media when “apply to entire duration” is active.", async () => {
+    // Select the Text
+    await Common_function.clickElementByXPath(
+      '//android.widget.FrameLayout[@resource-id="com.myzesty:id/container"]'
+    );
+    await WizardText.Apply_Text_Duration(); // Apply to entire duration
+    await WizardText.Apply_Changes();
+    await browser.pause(1000);
+    await Slider.play_pause_xpath(
+      '//android.widget.ImageView[@resource-id="com.myzesty:id/play"]'
+    );
+    await browser.pause(5000);
+    await Slider.play_pause(534, 1412);
+    await browser.pause(500);
+  });
+
+  it.only("Verify that text can be applied to only one media when circle icon is toggled to single mode.", async () => {
+    // Select the Media
+    await Common_function.clickElementByXPath(
+      '(//android.view.View[@resource-id="com.myzesty:id/selectedBg"])[2]'
+    );
+    // Select the Text
+    await Common_function.clickElementByXPath(
+      '//android.widget.FrameLayout[@resource-id="com.myzesty:id/container"]'
+    );
+    await WizardText.Apply_Text_Duration(); // Apply to current media only
+    await WizardText.Apply_Changes();
+    await browser.pause(1000);
+    // Select 3rd Media
+    await Common_function.clickElementByXPath(
+      '(//android.view.View[@resource-id="com.myzesty:id/selectedBg"])[3]'
+    );
+    async function Validate_text_On_Media() {
+      const isVisible = await Common_function.waitForElementToDisappear(
+        // Verify that text is not present on 3rd media
+        '//android.widget.FrameLayout[@resource-id="com.myzesty:id/container"]'
+      );
+      if (!isVisible) {
+        console.log("Text is not present on next media as expected.");
+      } else {
+        throw new Error("Text is present on next media, test failed.");
+      }
+    }
+    await Validate_text_On_Media();
+  });
+
+  it("", async () => {});
+
+  it("", async () => {});
+
+  it("", async () => {});
+
+  // Removed empty test cases
+});
